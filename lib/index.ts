780920ba044e94475831191c0c2624a38bef2faa
Julius <njmwas@gmail.com>
2026-09-03 21:06:49 +0300

    Export the types as well

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,3 @@
 export { $, DomEntity } from "./dom";
 export { default, subscribe, useState } from "./state";
-// export { zElement, zElementGen, TAGS } from "./@types/zdom";
+export { type zElement, type zElementGen, TAGS, type tags } from "./@types/zdom";
